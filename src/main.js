da85c6fd3cbf6da5685e1a4640537cdf93426416
Sagar Parmar <sagar.parmar@hypd.in>
2023-01-04 00:34:43 +0530

    change sdk token

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ app.config.globalProperties = {
   $clientId: "ac9159c4fb2b4c6b94ff2f7a6d04abb6",
   $clientSecret: "45b60a7f490945e9868eaf17339229d4",
   $sdkToken:
-    "BQDckoC8Vx1Qtf1zAYnTr3LQvpgxmZptfN_pVO7jdqcIFwpCKlN4jQLhrC2C4aZqc9atA3WugGJd-YJz3YchHHcu2pLCsUcKIT84KhosWXE200XN9X581mccQFCCAczgL2NCdOoO-ndjTkF2qr9ZlicY8NuFeoqGclxOiSIJGikTio9KuW7ymrrjUUmDmJlewzcYxy-Hnfjk4_RQUCGpPSMNBYw",
+    "BQC-nYd3h6ruD2l2cpy0rGvpWgMYHUYN4E-aIjHRX6G10R4QPK-VckHIMThQfboKSaOUIrLeuTLZEDfyPHVoHo8xYgd5I3dXQk4HiLV4YqzUJ70vKKTw0sD_Q9PelF_gXKuUmBxGS46xYyzOwlGiDbALsC_PRWm6_EIibGS0GNsdQcnlJVJc6Ix4gifnQoH5cVNvdc7IFT0cCHN4ijzcplE3wVk",
   $extractArtistNames: (artists) => {
     let artistNames = "";
     artists.forEach((artist) => {
